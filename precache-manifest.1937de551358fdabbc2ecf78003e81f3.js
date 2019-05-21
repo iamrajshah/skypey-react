c4ae64ee3887075e9ba408085244b58dcfd7a473
iamrajshah <shahrajesh2113@yahoo.com>
2019-05-21 15:38:17 +0530

    Updates

---
+++ precache-manifest.1937de551358fdabbc2ecf78003e81f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca3bbb77325dbf18d7987a92363f8ce",
+    "url": "/skypey-react/index.html"
+  },
+  {
+    "revision": "58479b20d573cf7e5e55",
+    "url": "/skypey-react/static/css/main.75ad689a.chunk.css"
+  },
+  {
+    "revision": "ab3a40faf3b1dde8ada5",
+    "url": "/skypey-react/static/js/2.4719951f.chunk.js"
+  },
+  {
+    "revision": "58479b20d573cf7e5e55",
+    "url": "/skypey-react/static/js/main.cdcb8eed.chunk.js"
+  },
+  {
+    "revision": "d4acdd816e07999878af",
+    "url": "/skypey-react/static/js/runtime~main.1e80593c.js"
+  }
+]);
